86b8052d6a77a2429b4db0fb8705a1cf99f75ecc
bergamotBen <bergamotBen@icloud.com>
2023-01-17 16:29:39 +0000

    task 5 completed and all tests passing

--- __tests__/app.test.js
+++ __tests__/app.test.js
@@ -80,49 +80,47 @@ describe("GET /api/articles", () => {
       });
   });
 });
-const app = require("../app");
-const request = require("supertest");
-const seed = require("../db/seeds/seed");
-const data = require("../db/data/test-data");
-const db = require("../db/connection");
 
-beforeEach(() => {
-  return seed(data);
-});
-
-afterAll(() => {
-  db.end();
-});
-
-describe("GET /api/topics", () => {
-  it("returns a status 200 and responds with all topics.", () => {
-    return request(app)
-      .get("/api/topics")
-      .expect(200)
-      .then(({ body }) => {
-        expect(body.topics).toBeInstanceOf(Array);
-      });
-  });
-  it("returns objects with slug and description keys.", () => {
-    return request(app)
-      .get("/api/topics")
-      .expect(200)
-      .then(({ body }) => {
-        expect(body.topics.length).not.toBe(0);
-        expect(body.topics[0]).toHaveProperty("slug");
-        expect(body.topics[0]).toHaveProperty("description");
-      });
-  });
-});
-
-describe.only("GET /api/articles/:article_id", () => {
+describe("GET /api/articles/:article_id", () => {
   it("returns 200 and the requested article", () => {
     return request(app)
       .get("/api/articles/1")
       .expect(200)
       .then(({ body }) => {
-        expect(body).toBeInstanceOf(Array);
-        //this test is incomplete//
+        expect(body.article).toBeInstanceOf(Object);
+        expect(body.article).toHaveProperty("article_id");
+        expect(body.article).toHaveProperty("title");
+        expect(body.article).toHaveProperty("topic");
+        expect(body.article).toHaveProperty("author");
+        expect(body.article).toHaveProperty("body");
+        expect(body.article).toHaveProperty("created_at");
+        expect(body.article).toHaveProperty("votes");
+        expect(body.article).toHaveProperty("article_img_url");
+
+        expect(body.article.article_id).toBe(1);
+        expect(typeof body.article.title).toBe("string");
+        expect(typeof body.article.topic).toBe("string");
+        expect(typeof body.article.author).toBe("string");
+        expect(typeof body.article.body).toBe("string");
+        expect(typeof body.article.created_at).toBe("string");
+        expect(typeof body.article.votes).toBe("number");
+        expect(typeof body.article.article_img_url).toBe("string");
+      });
+  });
+  it("returns 400 when given an invalid :article_id", () => {
+    return request(app)
+      .get("/api/articles/article")
+      .expect(400)
+      .then(({ body }) => {
+        expect(body.message).toBe("Bad request");
+      });
+  });
+  it.only("returns 404 when given a valid but nonexistent :article_id", () => {
+    return request(app)
+      .get("/api/article/123")
+      .expect(404)
+      .then(({ body }) => {
+        expect(body.message).toBe("Not found.");
       });
   });
 });
--- models/models.js
+++ models/models.js
@@ -20,4 +20,25 @@ const readArticles = () => {
     });
 };
 
-module.exports = { readTopics, readArticles };
+const readArticle = (articleId) => {
+  return db
+    .query(
+      `
+SELECT * FROM articles WHERE article_id=$1`,
+      [articleId.article_id]
+    )
+    .then(({ rows }) => {
+      //   if (!rows.length === 0) {
+      console.log(rows);
+      return { article: rows[0] };
+      //   }
+      //   return Promise.reject({
+      //     status: 404,
+      //     message: "article_id does not exist",
+      //   });
+    })
+    .catch(() => {
+      return Promise.reject({ status: 400, message: "Bad request" });
+    });
+};
+module.exports = { readTopics, readArticles, readArticle };
