4e1bcf6addc57a53a1126060aa79bb4457164cc2
bergamotBen <bergamotBen@icloud.com>
2023-01-20 11:09:46 +0000

    api hosting complete

--- db/connection.js
+++ db/connection.js
@@ -1,12 +1,20 @@
-const { Pool } = require('pg');
-const ENV = process.env.NODE_ENV || 'development';
+const { Pool } = require("pg");
+const ENV = process.env.NODE_ENV || "development";
 
-require('dotenv').config({
+require("dotenv").config({
   path: `${__dirname}/../.env.${ENV}`,
 });
 
-if (!process.env.PGDATABASE) {
-  throw new Error('PGDATABASE not set');
+if (!process.env.PGDATABASE && !process.env.DATABASE_URL) {
+  throw new Error("PGDATABASE or DATABASE_URL not set");
 }
 
-module.exports = new Pool();
+const config =
+  ENV === "production"
+    ? {
+        connectionString: process.env.DATABASE_URL,
+        max: 2,
+      }
+    : {};
+
+module.exports = new Pool(config);
