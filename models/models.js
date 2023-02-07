4af190cf0b0248dc618400138dee080d55eb66fc
bergamotBen <bergamotBen@icloud.com>
2023-02-07 15:59:31 +0000

    refactored comments to return by DESC

--- models/models.js
+++ models/models.js
@@ -75,7 +75,8 @@ const readCommentsByArticleId = (article_id) => {
   return db
     .query(
       `SELECT * FROM comments
-      WHERE article_id=$1`,
+      WHERE article_id=$1
+      ORDER BY comments.created_at DESC;`,
       [article_id]
     )
     .then((comments) => {
